5993b9eb347b260f1dbc36c4b40707ba6faf973c
Jussi <dejaru@gmail.com>
2017-06-27 10:08:16 +0300

    Added API base uri to dev variables.

--- template/config/dev.env.js
+++ template/config/dev.env.js
@@ -2,5 +2,7 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  PORT: '8080',
+  API_BASE_URL: '"http://localhost:8080"',
 })
